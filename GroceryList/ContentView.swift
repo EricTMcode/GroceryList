2f4003dd07f019cec35159555eadc504d2aaf2e1
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-20 17:30:09 +0100

    Create and save a new item

--- GroceryList/ContentView.swift
+++ GroceryList/ContentView.swift
@@ -12,6 +12,15 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
+    @State private var item = ""
+
+    private func addEssentialFoods() {
+        modelContext.insert(Item(title: "Milk", isCompleted: true))
+        modelContext.insert(Item(title: "Butter", isCompleted: false))
+        modelContext.insert(Item(title: "Breed", isCompleted: .random()))
+        modelContext.insert(Item(title: "Coffee", isCompleted: .random()))
+        modelContext.insert(Item(title: "Beer", isCompleted: .random()))
+    }
 
     var body: some View {
         NavigationStack {
@@ -23,9 +32,51 @@ struct ContentView: View {
                         .foregroundStyle(item.isCompleted == false ? Color.primary : Color.accentColor)
                         .strikethrough(item.isCompleted)
                         .italic(item.isCompleted)
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                modelContext.delete(item)
+                                try? modelContext.save()
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button("Done", systemImage: item.isCompleted == false ? "checkmark.circle" : "x.circle") {
+                                item.isCompleted.toggle()
+                                try? modelContext.save()
+                            }
+                            .tint(item.isCompleted == false ? .green : .accentColor)
+                        }
                 }
             }
             .navigationTitle("Grecery List")
+            .toolbar {
+                if items.isEmpty {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            addEssentialFoods()
+                        } label: {
+                            Label("Essentials", systemImage: "carrot")
+                        }
+                    }
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
+                VStack {
+                    TextField("", text: $item)
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+                        let newItem = Item(title: item, isCompleted: false)
+                        modelContext.insert(newItem)
+                        try? modelContext.save()
+                        item = ""
+                    } label: {
+                        Text("Save")
+                    }
+                }
+                .padding()
+            }
             .overlay {
                 if items.isEmpty {
                     ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to the shopping list."))
