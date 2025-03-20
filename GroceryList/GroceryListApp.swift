697d6139321d27aec34bba38e997d1f87ce5d8b0
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-20 16:26:47 +0100

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Eric on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
